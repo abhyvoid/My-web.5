7d53031ccafb7a390daa0f94a02d911ce803085a
abhyvoid <abhayrangappanvat@gmail.com>
2025-08-22 20:13:44 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,34 @@
 // Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, onSnapshot, collection, deleteDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-// Config (your own Firebase keys)
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  onSnapshot, 
+  collection, 
+  deleteDoc, 
+  updateDoc, 
+  increment, 
+  addDoc 
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+import { 
+  getStorage, 
+  ref, 
+  uploadBytes, 
+  getDownloadURL, 
+  deleteObject 
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
+
+// ✅ Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBjSQoODv0AoPsExhsbLrYTyP90LOJX_9Y",
   authDomain: "my-web4o.firebaseapp.com",
@@ -13,8 +38,32 @@ const firebaseConfig = {
   appId: "1:226593866449:web:adbdf758ed4810d8d9fbf0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export services
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, db, doc, setDoc, getDoc, onSnapshot, collection, deleteDoc, updateDoc, increment };
+export { 
+  auth, 
+  signInWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut, 
+  db, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  onSnapshot, 
+  collection, 
+  deleteDoc, 
+  updateDoc, 
+  increment, 
+  addDoc,
+  storage, 
+  ref, 
+  uploadBytes, 
+  getDownloadURL, 
+  deleteObject 
+};
