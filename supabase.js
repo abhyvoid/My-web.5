47c59fae7f68c8fecc88e5b4d585a691538fe07c
abhyvoid <abhayrangappanvat@gmail.com>
2025-08-23 20:28:00 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,4 @@
-// supabase.js
-const supabaseUrl = "https://kfgjkkprfppnewhehzfr.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmZ2pra3ByZnBwbmV3aGVoemZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NTE0MTEsImV4cCI6MjA3MTUyNzQxMX0.OnfuZGz_c6SglErHOI6vGHt-Y44QHkThWXbZPw4ieSs";
+const SUPABASE_URL = "https://kfgjkkprfppnewhehzfr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmZ2pra3ByZnBwbmV3aGVoemZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NTE0MTEsImV4cCI6MjA3MTUyNzQxMX0.OnfuZGz_c6SglErHOI6vGHt-Y44QHkThWXbZPw4ieSs";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
-
-export { supabase };
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
